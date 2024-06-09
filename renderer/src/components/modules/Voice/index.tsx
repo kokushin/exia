5f7b49123236e4558d4b04b5b8c15fbd861963bd
Yusuke Ishiguro <mail@kokush.in>
2024-06-10 02:57:09 +0900

    feat: voiceコンポーネント 追加

--- renderer/src/components/modules/Voice/index.tsx
+++ renderer/src/components/modules/Voice/index.tsx
@@ -1,2 +1,18 @@
-// wav再生用コンポーネント
+import { useRecoilValue } from "recoil";
+import { scenarioState } from "@/states/scenarioState";
 import { CONFIG } from "@/constants";
+
+export const Voice: React.FC = () => {
+  const scenario = useRecoilValue(scenarioState);
+
+  if (
+    !CONFIG.VOICEVOX ||
+    !scenario.currentLine ||
+    scenario.currentLine.character === undefined ||
+    scenario.currentLine.type !== 1
+  ) {
+    return null;
+  }
+
+  return <audio src={`./audios/voices/${scenario.id}_${scenario.currentLineIndex}.wav`} autoPlay className="hidden" />;
+};
