6127cbc82fe1af27d0a33d119d99f1c2639ec7a8
Yusuke Ishiguro <mail@kokush.in>
2024-06-11 19:15:08 +0900

    hotfix

--- renderer/src/components/modules/Loading/index.tsx
+++ renderer/src/components/modules/Loading/index.tsx
@@ -55,7 +55,7 @@ export const Loading: React.FC = () => {
       className={`flex justify-center items-center absolute top-0 left-0 z-50 w-full h-full bg-black ${
         isLoaded ? "pointer-events-none" : ""
       }`}
-      style={{ opacity: isLoaded ? 1 : 1, transition: `opacity ${FADE_DURATION}ms ease` }}
+      style={{ opacity: isLoaded ? 0 : 1, transition: `opacity ${FADE_DURATION}ms ease` }}
     >
       <div className="relative w-8 h-8 animate-loading-spin">
         <div className="absolute top-0 left-0 w-4 h-4 rotate-45 before:content-[''] before:w-4 before:h-[2px] before:bg-white before:absolute before:top-1/2 before:left-1/2 before:-translate-x-1/2 before:-translate-y-1/2 after:content-[''] after:w-[2px] after:h-4 after:bg-white after:absolute after:top-1/2 after:left-1/2 after:-translate-x-1/2 after:-translate-y-1/2 animate-loading-effect-odd" />
